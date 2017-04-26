13af5bcf18349c33b4f48345608481bab46d031b
Humphrey Ahn <ahnsv@bc.edu>
2017-04-26 11:11:17 -0400

    04/26 commit

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import img from './sf.jpg';
+import background1 from '../public/background1.jpg';
 import './App.css';
 
 class App extends Component {
@@ -7,22 +7,64 @@ class App extends Component {
   constructor(){
     super();
     this.state = {city: 'San Francisco', weather: '27'};
+}
+
+  componentWillMount() {//most useful?
+   // Called  the first time the component is loaded right before the component is added to the page
+   console.log('getting cwm ...' + this.state);
+
+   //   Api.get('example.com/api/users').then((data) => {
+   //listen();
+   //   });
   }
 
-  clicked(val) {
-    this.setState({weather: val})
-    console.log(val);
+  componentDidMount() {
+     // Called after the component has been rendered into the page
+     console.log('getting cdm...' + this.state);
+     //var button = document.getElementById('button');
+     //debugger;
   }
+
+  componentWillReceiveProps(nextProps) {
+     // Called when the props provided to the component are changed
+     console.log(
+        'getting cwr...' + this.state
+     );
+     if(this.props.userId !== nextProps.userId) {
+        //make api call
+     }
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+     // Called when the proprs and/or state change
+     console.log('updating...');
+     console.log(nextState.text);
+  }
+
+  componentWillUnmount() {
+     //unlisten();
+  }
+  clicked(val) {
+     console.log(this.state);
+    this.setState({
+      weather: val});
+    //this.forceUpdate();
+    //console.log(val);
+}
 
   render() {
     var self = this;
+    console.log('rendering');
     return (
       <div className="App">
+        <img src={background1} />
         <div className="App-header">
-          <div className="Temp-text"> {this.state.weather}&#176; </div>
+          <div className="Temp-text"> {this.state.weather}&#176;</div>
           <div className="City-text"> {this.state.city} </div>
         </div>
         <div className="App-body">
+          <input ref="textBox" placeholder="Enter City Name" type="text" />
+          <button onClick={ (e) => {this.clicked();}}>Submit</button>
           <div className="Day-item" onClick={self.clicked.bind(self, 27)}>
             <div className="Day-text"> Mon </div>
             <div className="Day-temp"> 27&#176; </div>
