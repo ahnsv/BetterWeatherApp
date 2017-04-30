6164a3ebfeab9ac534ca21be69ad9b22712f8f3a
Humphrey Ahn <ahnsv@bc.edu>
2017-04-30 10:56:25 -0400

    04/30 commit
    Add forecast and other stuffs

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.css";
 import "bootswatch/cosmo/bootstrap.css";
 import { Navbar, NavItem, Nav, Grid, Row, Col } from "react-bootstrap";
-import WeatherIcons from 'react-weathericons';
+import icon from './images/cloud.png';
+import $ from 'jquery';
+import './App.css';
 
 
 class App extends Component {
-  constructor(props) {
+   constructor(props) {
     super(props);
     this.state = {
       city: undefined,
@@ -15,13 +17,49 @@ class App extends Component {
       humidity: undefined,
       wind: undefined,
       format: 'C',
-      weather: ''
+      weather: '',
+      toggle: true,
+      index: 1,
+      pressure: undefined,
+      visibility: undefined,
+      clouds: undefined,
+      sunrise: undefined,
+      sunset: undefined,
+      forecast: {
+         day1: undefined,
+         day2: undefined,
+         day3: undefined,
+         day4: undefined,
+         day5: undefined,
+         day6: undefined
+      }
     };
+    this._toggleDiv = this._toggleDiv.bind(this);
+    this.fetchWeather = this.fetchWeather.bind(this);
   }
-  static defaultProps = {
+
+   _toggleDiv() {
+    $(this.refs['toggle-div']).slideToggle();
+  }
+
+  clicked(val, index) {
+    if (this.state.toggle == true && this.state.index == index) {
+      this.setState({toggle: false});
+      this._toggleDiv();
+    }
+    if (this.state.toggle == false && this.state.idex != index) {
+      this.setState({toggle: true});
+      this._toggleDiv();
+    }
+    this.setState({weather: val, index: index});
+    console.log(val);
+  }
+
+   static defaultProps = {
       city: 'Boston',
     };
- _getWeatherInfo = (city) => {
+
+   _getWeatherInfo = (city) => {
      const main = this;
      let query = null;
      main.setState({
@@ -54,7 +92,12 @@ class App extends Component {
          high: data.main.temp_max,
          humidity: data.main.humidity,
          wind: data.wind.speed,
-         weather: data.weather[0]
+         weather: data.weather[0],
+         pressure: data.main.pressure,
+         visibility: data.visibility,
+         clouds: data.clouds.all,
+         sunrise: data.sys.sunrise,
+         sunset: data.sys.sunset
        });
      })
      .catch( function() {
@@ -64,22 +107,85 @@ class App extends Component {
      })
    };
 
+   fetchWeather = (city) => {
+      const main = this;
+      let query = null;
+      main.setState({
+          infoStatus: 'loading'
+      });
+      if (!city || city == '') {
+        query = this.props.city;
+      } else {
+        query = city ;
+      }
+      fetch(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${query},us&units=metric&cnt=7&appid=50a34e070dd5c09a99554b57ab7ea7e2`)
+      .then( function(response) {
+        return response;
+      })
+      .then( function(response) {
+        setTimeout( function() {
+          main.setState({
+          infoStatus: 'loaded'
+        });
+        }, 300);
+      //   console.log(response.json());
+        return response.json();
+      })
+      .then((response) => {
+           const data = main.state.forecast;
+         //   console.log(response.list);
+           main.setState({
+             forecast: {
+                day1: response.list[0].temp.day,
+                day2: response.list[1].temp.day,
+                day3: response.list[2].temp.day,
+                day4: response.list[3].temp.day,
+                day5: response.list[4].temp.day,
+                day6: response.list[5].temp.day,
+          }})
+       })
+      .catch( function() {
+         main.setState({
+           infoStatus: 'error'
+         });
+      });
+    }
+
    changeFormat(format) {
 
        let temperature = 0;
        let low = 0;
        let high = 0;
+       let day1 = 0;
+       let day2 = 0;
+       let day3 = 0;
+       let day4 = 0;
+       let day5 = 0;
+       let day6 = 0;
+       let day7 = 0;
        let newFormat = '';
 
        if (format === 'C') {
          temperature = (this.state.temperature * (9/5) + 32).toFixed(2);
          low = (this.state.low * (9/5) + 32).toFixed(2);
          high = (this.state.high * (9/5) + 32).toFixed(2);
+         day1 = (this.state.forecast.day1 * (9/5) + 32).toFixed(2);
+         day2 = (this.state.forecast.day2 * (9/5) + 32).toFixed(2);
+         day3 = (this.state.forecast.day3 * (9/5) + 32).toFixed(2);
+         day4 = (this.state.forecast.day4 * (9/5) + 32).toFixed(2);
+         day5 = (this.state.forecast.day5 * (9/5) + 32).toFixed(2);
+         day6 = (this.state.forecast.day6 * (9/5) + 32).toFixed(2);
          newFormat = 'F';
        } else {
          temperature = ((this.state.temperature - 32) * (5/9)).toFixed(2);
          low = ((this.state.low - 32) * (5/9)).toFixed(2);
          high = ((this.state.high - 32) * (5/9)).toFixed(2);
+         day1 = ((this.state.forecast.day1 - 32) * (5/9)).toFixed(2);;
+         day2 = ((this.state.forecast.day2 - 32) * (5/9)).toFixed(2);;
+         day3 = ((this.state.forecast.day3 - 32) * (5/9)).toFixed(2);;
+         day4 = ((this.state.forecast.day4 - 32) * (5/9)).toFixed(2);;
+         day5 = ((this.state.forecast.day5 - 32) * (5/9)).toFixed(2);;
+         day6 = ((this.state.forecast.day6 - 32) * (5/9)).toFixed(2);;
          newFormat = 'C';
        }
 
@@ -87,79 +193,149 @@ class App extends Component {
          format: newFormat,
          temperature: temperature,
          low: low,
-         high: high
-       });
+         high: high,
+         forecast: {
+            day1: day1,
+            day2: day2,
+            day3: day3,
+            day4: day4,
+            day5: day5,
+            day6: day6,
+         }
+       })
      }
 
 
-      componentWillMount() {
-          this._getWeatherInfo();
-        };
-        _handleSubmit = (event) => {
-          event.preventDefault();
-          this._getWeatherInfo(event.target.search.value);
-        };
+   componentWillMount() {
+       this._getWeatherInfo();
+       this.fetchWeather();
+     };
+   _handleSubmit = (event) => {
+    event.preventDefault();
+    this._getWeatherInfo(event.target.search.value);
+    this.fetchWeather(event.target.search.value);
+   };
   render() {
     let hr = (new Date).getHours()
     let tod = (hr >= 17) ? 'night' : 'day';
-    const {
-          city,
-          country,
-          description,
-          temperature,
-          low,
-          high,
-          humidity,
-          wind,
-          infoStatus,
-          format,
-          weather
-        } = this.state;
-        let data = null;
-        if (infoStatus == 'loaded') {
-         data = <div>
-             <h1>
-               {description} in {city} <i className={'wi wi-owm-' + tod + '-' + this.state.weather.id}></i>
-             </h1>
+    var self = this;
 
-             <h2>Current: {temperature} {format}</h2>
-             <h3>Low: {low}{format} High: {high}{format}</h3>
-             <p>Humidity: {humidity}%  </p>
-             <p>Wind Speed: {wind} mi/hr  </p>
-             {temperature &&
-          <SwitchFormat changeFormat={this.changeFormat.bind(this)} format={format} />}
-           </div>
-        } else if (infoStatus == 'loading') {
-          data = <div className="info loading">Loading weather data...</div>
-        } else if (infoStatus == 'error') {
-          data = <div className="info error">Error while loading weather data. Try again later.</div>
-        }
+    var appHeader = {
+      // backgroundImage: `url(sf.jpg)`,
+      backgroundSize: "cover",
+      position: "absolute",
+      display: "flex",
+      flexDirection: "row",
+      height: "100%",
+      width: "100%",
+      padding: "20px"
+   };
+    var iconStyle = {
+      width: "50px",
+      height: "50px",
+      alignSelf: "center",
+      backgroundImage: `url(${icon})`,
+      backgroundRepeat: "no-repeat"
+    };
+    // Create a new JavaScript Date object based on the timestamp
+   // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+    const {city, country, description, temperature, low, high, humidity, wind, infoStatus, format, weather, pressure, visibility, clouds, sunrise, sunset, forecast} = this.state;
+   //  const {day1, day2, day3, day4, day5} = this.state.forecast;
+    const day1 = this.state.forecast.day1;
+    const day2 = this.state.forecast.day2;
+    const day3 = this.state.forecast.day3;
+    const day4 = this.state.forecast.day4;
+    const day5 = this.state.forecast.day5;
+    const day6 = this.state.forecast.day6;
+
+     var date = new Date(sunrise*1000);
+     var date2 = new Date(sunset*1000);
+     // Hours part from the timestamp
+     var hours = date.getHours();
+     var hours2 = date2.getHours();
+     // Minutes part from the timestamp
+     var minutes = "0" + date.getMinutes();
+     var minutes2 = "0" + date2.getMinutes();
+
+     // Will display time in 10:30:23 format
+     var formattedTime = hours + ':' + minutes.substr(-2);
+     var formattedTime2 = hours2 + ':' + minutes2.substr(-2);
+     this.state.sunrise = formattedTime;
+     this.state.sunset = formattedTime2;
+
+     let data = null;
+     if (infoStatus == 'loaded') {
+      data = <div>
+          <h1>
+            {description} in {city}, {country} <i className={'wi wi-owm-' + tod + '-' + this.state.weather.id}></i><p>Current: {temperature} ˚{format}</p>
+          </h1>
+          <h3>Low: {low} ˚{format} High: {high} ˚{format}</h3>
+          <p>Humidity: {humidity}%  </p>
+          <p>Wind Speed: {wind} mi/hr  </p>
+          <p>Pressure: {pressure} mb  </p>
+          <p>Visibility: {visibility} m </p>
+          <p>Clouds: {clouds}  </p>
+          <p>Sunrise: {sunrise} </p>
+          <p>Sunset: {sunset} </p>
+          {temperature &&
+       <SwitchFormat changeFormat={this.changeFormat.bind(this)} format={format} />}
+        </div>
+     } else if (infoStatus == 'loading') {
+       data = <div className="info loading">Loading weather data...</div>
+     } else if (infoStatus == 'error') {
+       data = <div className="info error">Error while loading weather data. Try again later.</div>
+     }
+   //   console.log({forecast});
     return (
-      <div className="weatherApp">
-         <Navbar>
-             <Navbar.Header>
-               <Navbar.Brand>
-                 Location-Based Weather App
-               </Navbar.Brand>
-             </Navbar.Header>
-          </Navbar>
-         <Grid>
-            <Row>
-                <div className="weatherQuery">
-                      <h1>Search a city<span>
-                      <form onSubmit={this._handleSubmit}>
-                        <input
-                          type="text"
-                          name="search"
-                          placeholder="Search a City..."
-                        />
-                      </form>
-                      </span>
-                      </h1>
+      <div className="App">
+          <div className="App-header" style={appHeader}>
+            <div className="City-text">
+              <div className="weatherQuery">
+                    <h1><span>
+                    <form onSubmit={this._handleSubmit}>
+                      <input
+                        className="City-input"
+                        type="text"
+                        name="search"
+                        placeholder="Search a city"
+                      />
+                    </form>
+                    </span>
+                    </h1>
+                  {data}
+              </div>
+              <div className="App-body">
+                <div className="Day-item" onClick={self.clicked.bind(self, 27, 1)}>
+                  <div className="Day-text"> Mon </div>
+                  <div className="Day-temp"> {temperature}°{format} </div>
                 </div>
-                {data}
-            </Row>
-         </Grid>
+                <div className="Day-item" onClick={self.clicked.bind(self, 55, 2)}>
+                  <div className="Day-text"> Tue </div>
+                  <div className="Day-temp"> {day1}°{format} </div>
+                </div>
+                <div className="Day-item" onClick={self.clicked.bind(self, 45, 3)}>
+                  <div className="Day-text"> Wed </div>
+                  <div className="Day-temp"> {day2}°{format} </div>
+                </div>
+                <div className="Day-item" onClick={self.clicked.bind(self, 47, 4)}>
+                  <div className="Day-text"> Thu </div>
+                  <div className="Day-temp"> {day3}°{format} </div>
+                </div>
+                <div className="Day-item" onClick={self.clicked.bind(self, 52, 5)}>
+                  <div className="Day-text"> Fri </div>
+                  <div className="Day-temp"> {day4}°{format} </div>
+                </div>
+                <div className="Day-item" onClick={self.clicked.bind(self, 52, 5)}>
+                  <div className="Day-text"> Sat </div>
+                  <div className="Day-temp"> {day5}°{format} </div>
+                </div>
+                <div className="Day-item" onClick={self.clicked.bind(self, 52, 5)}>
+                  <div className="Day-text"> Sun </div>
+                  <div className="Day-temp"> {day6}°{format} </div>
+                </div>
+              </div>
+            </div>
+          </div>
       </div>
     );
   }
